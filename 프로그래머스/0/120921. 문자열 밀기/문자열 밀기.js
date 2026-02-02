8796695b88fa3e388120c691de2d43d11fe63568
Eun-Seok <dldmstjr114@naver.com>
2026-02-02 18:17:46 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,20 +1,4 @@
 function solution(A, B) {
-    if(A===B)return 0 
-    
-    let answer = 0;
-    let arrA = A.split('')
-    
-    for(let i=0;i<A.length;i++){
-        let lastA = arrA.pop()
-        arrA.unshift(lastA)
-        
-        let charA = arrA.join('')
-        
-        if(charA === B){
-            return i + 1
-        }
-    
-    }
-
-    return -1;;
+   let combined = B + B;
+    return combined.indexOf(A);
 }
