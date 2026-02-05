ade3a9e45b53a130a7f0287735933021f9383041
Eun-Seok <dldmstjr114@naver.com>
2026-02-06 03:09:13 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 28.52 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let answer = []
+    let lastPos = {}
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (lastPos[char] === undefined) {
+            answer.push(-1);
+        } else {
+            answer.push(i - lastPos[char]);
+        }
+
+        lastPos[char] = i;
+    }
+    return answer;
+}
