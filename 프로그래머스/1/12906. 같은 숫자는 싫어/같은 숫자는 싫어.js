58189216b46eacc22970a99bbae52dfd25cd977f
Eun-Seok <dldmstjr114@naver.com>
2026-02-20 23:19:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.81 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,8 +2,8 @@ function solution(arr)
 {
     let answer = []
     for(let i=0; i<arr.length; i++){
-        if(arr[i] !== arr[i+1]) answer.push(arr[i])
-    }
+        if(answer[answer.length - 1] !== arr[i]) answer.push(arr[i])
+    }      
     
     return answer;
 }
