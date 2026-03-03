44af8a7022f42e6df1ad27410da2a75c9b3f85b1
Eun-Seok <dldmstjr114@naver.com>
2026-03-03 20:02:39 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,15 +1,11 @@
 function solution(number) {
     let answer = 0
     for(let i=0; i<number.length; i++){
-        let own = number[i]
         
         for(let j=i+1; j<number.length; j++){
-            let two = number[j]
             
             for(let k=j+1; k<number.length; k++){
-                let three = number[k]
-                
-                if(own+two+three===0)answer++
+                if(number[i] + number[j] + number[k] === 0)answer++
             
             }
             
